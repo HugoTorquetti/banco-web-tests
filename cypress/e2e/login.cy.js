244c467c2e0a86e32b22bf0ef46fdaadd7d431d4
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-04-07 13:39:49 -0300

    adicionei o beforeEach para o acesso à pagina de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,10 @@
 describe('Login', () => {
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  beforeEach(() => {
     //Arrange
-    cy.visit('http://localhost:4000')
-    
+    cy.visit('http://localhost:4000')  
+  });
+
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Actions
     cy.get('#username')   //quando temos comandos muito complexos e que se extendem muito na mesma linha
       .click()            //passando de 80 caracteres, é recomendado quebrar a linha para melhorar a 
@@ -16,9 +18,6 @@ describe('Login', () => {
 
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    //Arrange
-    cy.visit('http://localhost:4000')
-    
     //Actions
     cy.get('#username')
       .click()            
