5132e965f14b74891d286237cca0d347ac0656f8
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-04-14 00:33:55 -0300

    Adicionei dados provenientes do fixture e alterei o .gitignore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,10 +7,13 @@ describe('Login', () => {
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Actions
-    cy.get('#username')   //quando temos comandos muito complexos e que se extendem muito na mesma linha
-      .click()            //passando de 80 caracteres, é recomendado quebrar a linha para melhorar a 
-      .type('julio.lima') // legibilidade do código, deixando cada comando em uma linha e identtado dessa forma
-    cy.get('#senha').click().type('123456')
+
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username')   //quando temos comandos muito complexos e que se extendem muito na mesma linha
+        .click()            //passando de 80 caracteres, é recomendado quebrar a linha para melhorar a 
+        .type(credenciais.valida.usuario) 
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.screenshot('preenchendo-os-campos-de-login')
     cy.get('#login-section > .btn').click()
     cy.screenshot('após-clicar-no-botão-entrar')
@@ -22,10 +25,10 @@ describe('Login', () => {
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     //Actions
-    cy.get('#username')
-      .click()            
-      .type('julio.lima') 
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.invalida.usuario) 
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button', 'Entrar').click() 
     /*A forma de selecionar o botão de entrar para o clique 
     pode ser feita assim, pois o código fica mais descritivo*/
