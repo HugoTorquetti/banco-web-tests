ebf9391ae67e5850b7d395582b7d181f64613af0
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-04-21 13:26:29 -0300

    Adicionando os testes que faltavam para cobertura de todas as regras de negócios de transferências.

--- cypress/e2e/transferencia.cy.js
+++ cypress/e2e/transferencia.cy.js
@@ -13,9 +13,35 @@ describe('Transferências', () => {
 
     });
 
+    it('Deve apresentar erro quando tentar transferir valor abaixo de 10', () => {
+        // Act
+        cy.realizarTransferencia('Heloisa', 'Hugo', '9.99');
+
+        // Assert
+        cy.validarToast('O valor da transferência deve ser maior ou igual a R$10,00.');
+
+    });
+
+    it('Deve transferir quando informo valor acima de 5000 e token válido', () => {
+        // Act
+        cy.realizarTransferencia('Heloisa', 'Hugo', '5000.01', '123456');
+
+        // Assert
+        cy.validarToast('Transferência realizada!');
+    });
+
     it('Deve apresentar erro quando tentar transferir valor acima de 5 mil e não informar token', () => {
         // Act
         cy.realizarTransferencia('Heloisa', 'Hugo', '5000.01');
+
+        // Assert
+        cy.validarToast('Autenticação necessária para transferências acima de R$5.000,00.');
+
+    });
+
+    it('Deve apresentar erro quando tentar transferir valor acima de 5 mil e informar token inválido', () => {
+        // Act
+        cy.realizarTransferencia('Heloisa', 'Hugo', '5000.01', '654321');
 
         // Assert
         cy.validarToast('Autenticação necessária para transferências acima de R$5.000,00.');
